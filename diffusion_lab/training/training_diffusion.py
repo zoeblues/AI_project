41548579cc897b339271ca386e51987acd82d6f6
Filip Dabkowski <filip.dabkowski@gmail.com>
2025-04-24 13:23:07 +0200

    remove unused function

--- diffusion_lab/training/training_diffusion.py
+++ diffusion_lab/training/training_diffusion.py
@@ -20,15 +20,6 @@ from diffusion_lab.datasets.dataset_diffusion import DiffusionDataset
 log = logging.getLogger(__name__)
 
 
-def get_grad_norm(model):
-	total_norm = 0.0
-	for p in model.parameters():
-		if p.grad is not None:
-			param_norm = p.grad.data.norm(2)
-			total_norm += param_norm.item() ** 2
-	return total_norm ** 0.5
-
-
 def train(model, scheduler, loader, optimizer, train_cfg, device):
 	model = model.to(device)  # to make sure it's on an intended device
 	criterion = nn.MSELoss()
