37052a220ce501379b62999506c9f419b0f3bc62
Filip Dabkowski <filip.dabkowski@gmail.com>
2025-04-26 23:42:54 +0200

    repair marge request, remove images, hopefully works

---
+++ diffusion_lab/sampling/sampling.py
@@ -0,0 +1,59 @@
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+
+from diffusion_lab.models.noise_scheduler import NoiseScheduler
+
+
+def sample_image(model, scheduler: NoiseScheduler, n_timesteps=1_000, n_images=1, resolution=(64, 64)):
+	model.eval()
+	x_t = torch.randn((n_images, 3, *resolution), device=model.device)  # B, C, W, H
+	with torch.no_grad():
+		for t in reversed(range(n_timesteps)):
+			t = torch.tensor([t], device=model.device)
+			epsilon = model(x_t, t)
+			x_t = scheduler.p_backward(x_t, epsilon, t)
+	return x_t
+
+
+if __name__ == '__main__':
+	from diffusion_lab.models.noise_scheduler import CosineNoiseScheduler
+	from diffusion_lab.models.diffusion import UNet
+	from omegaconf import DictConfig
+	
+	from torchvision.transforms import transforms
+	from PIL import Image
+	
+	cfg = DictConfig({
+		'base_channels': 64,
+		'image_size': 128,
+		'in_channels': 3,
+		'num_groups': 32,
+		'time_embedding_factor': 4,
+		'attention_heads': 4,
+		'out_channels': 3,
+		'num_layers': 2
+	})
+	
+	to_pil = transforms.ToPILImage()
+	
+	# Initialize the model
+	device = 'mps'
+	model = UNet(cfg, device=device)
+	model.load_state_dict(torch.load("outputs/steps/uncond_unet.pth", map_location='cpu'))
+	model.to(device)
+	scheduler = CosineNoiseScheduler(1000, device=model.device)
+	
+	x_t = torch.randn((1, 3, 64, 64), device=model.device)  # B, C, W, H
+	epsilon_t = model(x_t, torch.tensor([999], device=model.device))
+	x_0 = x_t - epsilon_t
+	
+	image_tensor = x_0[0]
+	
+	bgc = Image.new('RGB', (64 * 3, 64), color='white')
+	
+	bgc.paste(to_pil(x_t[0]), (0, 0))
+	bgc.paste(to_pil(epsilon_t[0]), (64, 0))
+	bgc.paste(to_pil(image_tensor), (128, 0))
+	bgc.show()
--- diffusion_lab/training/training_diffusion.py
+++ diffusion_lab/training/training_diffusion.py
@@ -37,9 +37,8 @@ def train(model, scheduler, loader, optimizer, train_cfg, device):
 			model.train()
 			optimizer.zero_grad()
 			
-			start_time = time.time()
-			
 			running_loss = 0.0
+			start_time = time.time()
 			progress_bar = tqdm(loader, desc=f"Epoch: {epoch + 1}/{train_cfg.params.epochs}", unit=" bch")
 			for i, batch in enumerate(progress_bar):
 				batch = batch.to(device)
@@ -64,15 +63,15 @@ def train(model, scheduler, loader, optimizer, train_cfg, device):
 			avg_loss = running_loss / len(loader.dataset)
 			elapsed = time.time() - start_time
 			it_per_sec = (len(loader.dataset) + 1) / elapsed
-			
-			mlflow.log_metric("epoch_loss", running_loss, step=epoch)
+
 			mlflow.log_metric("avg_loss", avg_loss, step=epoch)
-			mlflow.log_metric("it_per_sec", it_per_sec, step=epoch)
-			
-			if epoch % train_cfg.params.log_step == 0:
+			mlflow.log_metric("items_per_sec", it_per_sec, step=epoch)
+			mlflow.log_metric("elapsed", elapsed, step=epoch)
+
+			if (epoch + 1) % train_cfg.params.log_step == 0:
 				log.info(f"Epoch {epoch + 1}/{train_cfg.params.epochs} Loss: {avg_loss}")
-			if epoch % train_cfg.params.save_step == 0 and epoch != 0:
-				torch.save(model.state_dict(), pjoin(train_cfg.params.save_path, f"step-{epoch}.pth"))
+			if (epoch + 1) % train_cfg.params.save_step == 0 and epoch != 0:
+				torch.save(model.state_dict(), pjoin(train_cfg.params.save_path, f"step-{epoch + 1}.pth"))
 	
 	# Save model
 	torch.save(model.state_dict(), pjoin(train_cfg.params.save_path, f"{train_cfg.params.model_name}.pth"))
@@ -83,8 +82,8 @@ def main(cfg: DictConfig):
 	mlflow.set_experiment(cfg.project.name)
 	
 	train_transform = transforms.Compose([
-		transforms.Resize(128),  # todo: config
-		transforms.RandomResizedCrop((128, 128), scale=(0.8, 1.0), ratio=(0.8, 1.2)),
+		transforms.Resize(64),  # todo: config
+		transforms.RandomResizedCrop((64, 64), scale=(0.8, 1.0), ratio=(0.8, 1.2)),
 		transforms.RandomHorizontalFlip(p=0.5),
 		transforms.ToTensor(),  # Convert image to tensor
 		transforms.Normalize(  # Normalize RGB pixel values: [0, 255] -> [-1, 1]
@@ -99,7 +98,7 @@ def main(cfg: DictConfig):
 	# Loading model dynamically, based on config.
 	model_path, model_name = cfg.model.type.rsplit(".", maxsplit=1)  # get module path, and class name
 	model_cls = getattr(importlib.import_module(model_path), model_name)  # dynamic load a given class from a library
-	model = model_cls(**cfg.model.params, device=cfg.train.params.device)  # create an instance, with params from config
+	model = model_cls(cfg.model.params, device=cfg.train.params.device)  # create an instance, with params from config
 	# Loading already trained model weights, if specified in the config (load_timestep != 0)
 	if cfg.train.params.load_timestep != 0 and 'load_path' in cfg.train.params and 'load_timestep' in cfg.train.params:
 		model.load_state_dict(torch.load(pjoin(cfg.train.params.load_path, f"{cfg.train.params.load_timestep}.pth")))
