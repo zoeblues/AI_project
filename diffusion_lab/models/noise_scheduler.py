4e65f3d0a44bab7103fee0e8d8df1709a6d5bdf5
Filip Dabkowski <filip.dabkowski@gmail.com>
2025-05-04 01:58:15 +0200

    update noise schedullers

--- diffusion_lab/models/noise_scheduler.py
+++ diffusion_lab/models/noise_scheduler.py
@@ -6,6 +6,7 @@ class NoiseScheduler:
 	"""
 	Base class for noise scheduler. Implementing the methods for a forward noising process.
 	"""
+	
 	def __init__(self, n_timesteps=1000, device="cpu"):
 		"""
 		:param n_timesteps: 
@@ -13,17 +14,11 @@ class NoiseScheduler:
 		"""
 		self.T = n_timesteps
 		self.device = device
+		
 		self.betas = torch.zeros(n_timesteps, device=self.device)
 		self.alphas = torch.zeros(n_timesteps, device=self.device)
 		self.alpha_bars = torch.zeros(n_timesteps, device=self.device)
 		
-		# to be overwritten by children
-		self.sigma = torch.zeros(n_timesteps, device=self.device)
-		self.one_over_sqrt_alpha = torch.zeros(n_timesteps, device=self.device)
-		self.sqrt_alpha_bar = torch.zeros(n_timesteps, device=self.device)
-		self.sqrt_one_minus_alpha_bar = torch.zeros(n_timesteps, device=self.device)
-		self.one_m_alpha_over_sqrt_one_m_alpha_bar = torch.zeros(n_timesteps, device=self.device)
-	
 	def q_forward(self, x_0, t):
 		"""
 		Forward diffusion process, adding Gaussian noise to the image :math:`x_0`.
@@ -38,16 +33,25 @@ class NoiseScheduler:
 		"""
 		
 		epsilon = torch.randn_like(x_0, device=self.device)
-		x_t = self.sqrt_alpha_bar[t][:, None, None, None] * x_0 + self.sqrt_one_minus_alpha_bar[t][:, None, None, None] * epsilon
+		x_t = (torch.sqrt(self.alpha_bars[t][:, None, None, None]) * x_0 +
+		       torch.sqrt(1 - self.alpha_bars[t][:, None, None, None]) * epsilon)
 		
 		return x_t, epsilon
 	
 	def p_backward(self, x_t, epsilon_t, t):
 		z = torch.randn_like(x_t, device=self.device) if t > 1 else torch.zeros_like(x_t, device=self.device)
-		x_t_minus_noise = x_t - self.one_m_alpha_over_sqrt_one_m_alpha_bar[t][:, None, None, None] * epsilon_t
-		x_t_minus_one = self.one_over_sqrt_alpha[t][:, None, None, None] * x_t_minus_noise + self.sigma[t][:, None, None, None] * z
 		
-		return x_t_minus_one
+		beta_t = self.betas[t][:, None, None, None]
+		alpha_t = self.alphas[t][:, None, None, None]
+		alpha_bar = self.alpha_bars[t][:, None, None, None]
+		
+		x_t_minus_one = (x_t - (1 - alpha_t) / torch.sqrt(1 - alpha_bar) * epsilon_t) / torch.sqrt(alpha_t)
+		# x_t_minus_one = torch.clamp(x_t_minus_one, -1.0, 1.0)
+		
+		pred_x_0 = (x_t - (1 - alpha_bar) / torch.sqrt(1 - alpha_bar) * epsilon_t) / torch.sqrt(alpha_t)
+		# pred_x_0 = torch.clamp(pred_x_0, -1.0, 1.0)
+		
+		return x_t_minus_one + torch.sqrt(beta_t) * z, pred_x_0
 
 
 class CosineNoiseScheduler(NoiseScheduler):
@@ -55,6 +59,7 @@ class CosineNoiseScheduler(NoiseScheduler):
 	Scheduler for cosine schedule proposed in "Improved Denoising Diffusion Probabilistic Models".
 	Cosine schedule adds less noise at the initial and final timestamps, while staying linear in the middle.
 	"""
+	
 	def __init__(self, n_timesteps=1000, s=0.008, device="cpu"):
 		super().__init__(n_timesteps, device)
 		x = torch.linspace(0, n_timesteps, n_timesteps + 1, device=device)
@@ -69,40 +74,67 @@ class CosineNoiseScheduler(NoiseScheduler):
 		alphas = 1.0 - betas
 		alpha_bars = torch.cumprod(alphas, dim=0)
 		
-		# Final calculation to save
-		self.sigma = betas ** 0.5
-		self.one_over_sqrt_alpha = 1 / alphas ** 0.5
-		self.sqrt_alpha_bar = alpha_bars ** 0.5
-		self.sqrt_one_minus_alpha_bar = (1 - alpha_bars) ** 0.5
-		self.one_m_alpha_over_sqrt_one_m_alpha_bar = (1 - alphas) / self.sqrt_one_minus_alpha_bar
-
+		self.betas = betas
+		self.alphas = alphas
+		self.alpha_bars = alphas_bar
+		
 
 class LinearNoiseScheduler(NoiseScheduler):
 	"""
 	Scheduler for a linear beta schedule. Beta increases linearly from beta_start to beta_end.
 	"""
+	
 	def __init__(self, n_timesteps=1000, beta_start=0.0001, beta_end=0.02, device="cpu"):
 		super().__init__(n_timesteps, device)
-
+		
 		# Linearly spaced beta values
-		self.betas = torch.linspace(beta_start, beta_end, n_timesteps)
+		self.betas = torch.linspace(beta_start, beta_end, n_timesteps, device=self.device)
 		self.alphas = 1.0 - self.betas
 		self.alpha_bars = torch.cumprod(self.alphas, dim=0)
-
-		# Precompute squares for sampling
-		self.sqrt_alpha_bar = torch.sqrt(self.alpha_bars)
-		self.sqrt_one_minus_alpha_bar = torch.sqrt(1.0 - self.alpha_bars)
-		self.one_minus_alpha = 1.0 - self.alpha_bars
-
-		# Cache for reverse process
-		self.sqrt_recip_alphas = torch.sqrt(1.0 / self.alphas)
-		self.posterior_variance = (self.betas * (1.0 - torch.cat([torch.tensor([1.0]), self.alpha_bars[:-1]])) / (1.0 - self.alpha_bars))
-
-	
-
-def main():
-    scheduler = LinearNoiseScheduler()
-    print("First beta value:", scheduler.betas[0].item())
+		
 
 if __name__ == "__main__":
-    main()
+	from PIL import Image
+	from torchvision import transforms
+	
+	path = 'data/resized_images/Cat/cat-test_(1).jpeg'
+	image = Image.open(path).convert('RGB')
+	
+	train_transformation = transforms.Compose([
+		transforms.Resize(64),
+		# transforms.RandomResizedCrop((256, 256), scale=(0.8, 1.0), ratio=(0.9, 1.2)),
+		# transforms.RandomHorizontalFlip(p=0.5),
+		transforms.ToTensor(),  # Convert image to tensor
+		transforms.Normalize(  # Normalize RGB pixel values: [0, 1] -> [-1, 1]
+			mean=[0.5, 0.5, 0.5],
+			std=[0.5, 0.5, 0.5]
+		)
+	])
+	to_pil = transforms.Compose([
+		transforms.Normalize(  # Normalize RGB pixel values: [-1, 1] -> [0, 1]
+			mean=[-1.0, -1.0, -1.0],
+			std=[2.0, 2.0, 2.0]
+		),
+		transforms.ToPILImage(),
+	])
+	
+	bgc = Image.new('RGB', (64 * 11, 64 * 2), color='white')
+	
+	scheduler = LinearNoiseScheduler()
+	
+	x_0 = train_transformation(image)
+	x_0 = x_0.unsqueeze(0)
+	
+	bgc.paste(to_pil(x_0[0]), (0, 0))
+	
+	for t in range(0, 1000, 100):
+		# t = 600
+		
+		x_t, epsilon = scheduler.q_forward(x_0, torch.tensor([t]))
+		bgc.paste(to_pil(x_t[0]), (64 + 64*(t//100), 0))
+		
+		_, x_0_pred = scheduler.p_backward(x_t, epsilon, torch.tensor([t]))
+		bgc.paste(to_pil(x_0_pred[0]), (64 + 64*(t//100), 64))
+		
+	bgc.show()
+	
