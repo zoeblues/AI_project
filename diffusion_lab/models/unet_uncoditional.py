6d68f79cd448ea7f2deea43a491b5b5340c0a730
Filip Dabkowski <filip.dabkowski@gmail.com>
2025-05-17 14:48:05 +0200

    ResolutionLayerDown - done

--- diffusion_lab/models/unet_uncoditional.py
+++ diffusion_lab/models/unet_uncoditional.py
@@ -48,10 +48,39 @@ class ResBlock(nn.Module):
 		return self.skip(x) + h
 
 
-class ResolutionLayer(nn.Module):
-	def __init__(self, in_channels, out_channels, groups=8):
+class ResolutionLayerDown(nn.Module):
+	def __init__(self, in_channels, out_channels,
+	             embed_channels=256, norm_groups=8, dropout=0.2,
+	             n_blocks=2, init_downsample=True, do_self_attention=False):
 		super().__init__()
+		# For ease of use whit UNet skip-connections, downsampling is done at the beginning of Resolution layer.
+		# However not all Layers start with down sampling, example first Layer.
+		self.downsample = nn.MaxPool2d(2, stride=2) if init_downsample else None
+		
+		# Add variable number of ResBlocks on a single Resolution Layer
+		res_blocks = []
+		for i in range(n_blocks):
+			in_channels = in_channels if i == 0 else out_channels
+			res_blocks.append(ResBlock(
+				in_channels=in_channels,
+				out_channels=out_channels,
+				embed_channels=embed_channels,
+				norm_groups=norm_groups,
+				dropout=dropout,
+			))
+		self.res_blocks = nn.ModuleList(res_blocks)
 
+	def forward(self, x, t):
+		# Only subsample when specified in `__init__`
+		if self.downsample is not None:
+			x = self.downsample(x)
+		# Sequentially apply all ResBlock over input
+		for res_block in self.res_blocks:
+			x = res_block(x, t)
+		
+		return x
+		
+		
 
 class UNet(nn.Module):
 	def __init__(self, in_channels, out_channels, base_channels=32, channel_multipliers=(1, 2, 4, 8)):
