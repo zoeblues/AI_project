2e57e18f4b11d9d51a342b20c3548b971aaa6a30
Filip Dabkowski <filip.dabkowski@gmail.com>
2025-05-17 17:06:31 +0200

    UNet done-ish

--- diffusion_lab/models/unet_uncoditional.py
+++ diffusion_lab/models/unet_uncoditional.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class ResBlock(nn.Module):
-	def __init__(self, in_channels, out_channels, mid_channels=None, embed_channels=256, norm_groups=8, dropout=0.2):
+	def __init__(self, in_channels, out_channels, mid_channels=None, emb_channels=256, norm_groups=8, dropout=0.2):
 		super().__init__()
 		# Option to add more controlled channel change, but defaults to out_channels
 		if mid_channels is None:
@@ -16,13 +16,13 @@ class ResBlock(nn.Module):
 			nn.Conv2d(in_channels, mid_channels, kernel_size=3, stride=1, padding=1),
 		)
 		
-		self.embed_projection = nn.Sequential(
+		self.emb_projection = nn.Sequential(
 			nn.SiLU(),
-			nn.Linear(embed_channels, mid_channels),
+			nn.Linear(emb_channels, mid_channels),
 		)
 		
 		self.conv2 = nn.Sequential(
-			nn.GroupNorm(norm_groups, in_channels),
+			nn.GroupNorm(norm_groups, mid_channels),
 			nn.SiLU(),
 			nn.Dropout2d(dropout),
 			nn.Conv2d(mid_channels, out_channels, kernel_size=3, stride=1, padding=1),
@@ -41,7 +41,7 @@ class ResBlock(nn.Module):
 		# First: Group Normalization, Activation Function, Convolution
 		h = self.conv1(x)
 		# Add Timestep embedding
-		h = h + self.embed_projection(t)[:, :, None, None]
+		h = h + self.emb_projection(t)[:, :, None, None]
 		# Second: Group Normalization, Activation Function, Dropout, Convolution
 		h = self.conv2(h)
 		# Skip connection form the input with convolution output
@@ -72,14 +72,10 @@ class UpConv(nn.Module):
 
 
 class ResolutionLayerDown(nn.Module):
-	def __init__(self, in_channels, out_channels,
-	             embed_channels=256, norm_groups=8, dropout=0.2,
-	             n_blocks=2, do_downsample=True, do_self_attention=False):
+	def __init__(self, in_channels, out_channels, n_blocks=2,
+	             emb_channels=256, norm_groups=8, dropout=0.2,
+	             do_self_attention=False):
 		super().__init__()
-		# For ease of use whit UNet skip-connections, downsampling is done at the beginning of Resolution layer.
-		# However not all Layers start with down sampling, example first Layer.
-		self.downsample = DownConv() if do_downsample else None
-		
 		# Add variable number of ResBlocks on a single Resolution Layer
 		res_blocks = []
 		for i in range(n_blocks):
@@ -87,54 +83,126 @@ class ResolutionLayerDown(nn.Module):
 			res_blocks.append(ResBlock(
 				in_channels=in_channels,
 				out_channels=out_channels,
-				embed_channels=embed_channels,
+				emb_channels=emb_channels,
 				norm_groups=norm_groups,
 				dropout=dropout,
 			))
 		self.res_blocks = nn.ModuleList(res_blocks)
+		
+		self.downsample = DownConv()
 
 	def forward(self, x, t):
-		# Only subsample when specified in `__init__`
-		if self.downsample is not None:
-			x = self.downsample(x)
 		# Sequentially apply all ResBlock over input
 		for res_block in self.res_blocks:
 			x = res_block(x, t)
 		
-		return x
+		return self.downsample(x), x
 
 
 class ResolutionLayerUp(nn.Module):
-	def __init__(self, in_channels, out_channels, skip_channels,
-	             embed_channels=256, norm_groups=8, dropout=0.2,
-	             n_blocks=2, do_upsample=True, do_self_attention=False):
+	def __init__(self, in_channels, out_channels, skip_channels, n_blocks=2,
+	             emb_channels=256, norm_groups=8, dropout=0.2,
+	             do_self_attention=False):
 		super().__init__()
+		self.upsample = UpConv(in_channels)
+		
 		res_blocks = []
 		for i in range(n_blocks):
 			in_channels = in_channels + skip_channels if i == 0 else out_channels
 			res_blocks.append(ResBlock(
 				in_channels=in_channels,
 				out_channels=out_channels,
-				embed_channels=embed_channels,
+				emb_channels=emb_channels,
 				norm_groups=norm_groups,
 				dropout=dropout,
 			))
 		self.res_blocks = nn.ModuleList(res_blocks)
 		
-		# When upsampling, first perform the Up sapling to new resolution,
-		# then, apply learned convolution intended for smoothing and handling artefacts
-		self.upsample = UpConv(out_channels) if do_upsample else None
-	
-	def forward(self, x, x_skip, t):
+	def forward(self, x, skip_x, t):
+		x = self.upsample(x)
+		x = torch.cat([skip_x, x], dim=1)
 		for res_block in self.res_blocks:
 			x = res_block(x, t)
-		
-		if self.upsample is not None:
-			x = self.upsample(x)
 		
 		return x
 		
 
 class UNet(nn.Module):
-	def __init__(self, in_channels, out_channels, base_channels=32, channel_multipliers=(1, 2, 4, 8)):
+	def __init__(self, in_channels, out_channels, base_channels=320, channel_multipliers=(1, 2, 4, 4),
+	             emb_channels=256, norm_groups=8, dropout=0.2, device="cpu"):
 		super().__init__()
+		self.time_emb_channels = emb_channels
+		self.device = device
+		
+		self.initial_conv = nn.Conv2d(in_channels, base_channels, kernel_size=3, stride=1, padding=1)
+		
+		down_layers = []
+		prev_channels = base_channels
+		for i, multiply in enumerate(channel_multipliers):
+			down_layers.append(ResolutionLayerDown(
+				in_channels=prev_channels, out_channels=base_channels * multiply,
+				emb_channels=emb_channels, norm_groups=norm_groups, dropout=dropout,
+				do_self_attention=False  # todo: change
+			))
+			prev_channels = base_channels * multiply
+		self.down_layers = nn.ModuleList(down_layers)
+		
+		self.middle_block = nn.ModuleList([
+			ResBlock(prev_channels, prev_channels, emb_channels=emb_channels, norm_groups=norm_groups, dropout=dropout),
+			ResBlock(prev_channels, prev_channels, emb_channels=emb_channels, norm_groups=norm_groups, dropout=dropout),
+		])
+		
+		up_layers = []
+		for i, multiply in enumerate(reversed(channel_multipliers)):
+			up_layers.append(ResolutionLayerUp(
+				in_channels=prev_channels, out_channels=base_channels * multiply, skip_channels=base_channels * multiply,
+				emb_channels=emb_channels, norm_groups=norm_groups, dropout=dropout,
+				do_self_attention=False  # todo: change
+			))
+			prev_channels = base_channels * multiply
+		self.up_layers = nn.ModuleList(up_layers)
+		
+		self.output_conv = nn.Conv2d(base_channels, out_channels, kernel_size=1)
+	
+	def pos_encoding(self, t, channels):
+		inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2, device=self.device).float() / channels))
+		pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
+		pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
+		pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+		return pos_enc
+	
+	def forward(self, x, t):
+		# Encode timesteps
+		t = t.unsqueeze(-1).type(torch.float)
+		t = self.pos_encoding(t, self.time_emb_channels)
+		# Initial convolution to bring to base channels
+		x = self.initial_conv(x)
+		
+		# Backlog for skip connections
+		skip_connections = []
+		# Down-sampling the image
+		for layer in self.down_layers:
+			x, skip = layer(x, t)
+			skip_connections.append(skip)
+		
+		# Bottleneck
+		for block in self.middle_block:
+			x = block(x, t)
+		
+		# Reverse the skip connections
+		skip_connections = list(reversed(skip_connections))
+		# Up-sampling the features
+		for layer, skip_x in zip(self.up_layers, skip_connections):
+			x = layer(x, skip_x, t)
+		
+		# Return with output Conv
+		return self.output_conv(x)
+
+
+if __name__ == '__main__':
+	model = UNet(in_channels=3, out_channels=3, channel_multipliers=(1, 2, 4), base_channels=32, device="mps").to("mps")
+	model.eval()
+	
+	img = torch.randn(1, 3, 64, 64, device="mps")
+	t = (torch.ones(1) * 999).long().to("mps")
+	out = model(img, t)
